29052cfeae5c2878e89694da8807ba824fbc0d44
Md Nazmul Hossain <hossainmdnazmul268@gmail.com>
2023-06-10 18:53:07 +0600

    all instructor api is created

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ac-onjpk5k-shard-00-00.xskcn3u.mongodb.net:27017,ac-onjpk5k-shard-00-01.xskcn3u.mongodb.net:27017,ac-onjpk5k-shard-00-02.xskcn3u.mongodb.net:27017/?ssl=true&replicaSet=atlas-g07jbs-shard-0&authSource=admin&retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -35,6 +35,28 @@ async function run() {
         const result = await usersCollection.find().toArray()
         res.send(result)
     })
+
+    // get all instructor api 
+
+    app.get('/allInstructor', async(req,res)=> {
+      
+        const query = { role : "Instructor"}
+        const result = await usersCollection.find(query).toArray()
+        res.send(result)
+    })
+
+
+
+    // delete user api 
+
+    app.delete('/users/:id', async(req,res)=>{
+        const id = req.params.id 
+        const query = {_id : new ObjectId(id)}
+        const result = await usersCollection.deleteOne(query)
+        res.send(result)
+    })
+
+
   
     // admin role api  
     app.patch('/users/admin/:id', async(req,res)=>{
